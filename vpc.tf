3cbaba97bb51e0270a91cbef7fb22c05e92dd637
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-04-19 11:03:24 +0530

    terraform aws vpc module

--- vpc.tf
+++ vpc.tf
@@ -3,6 +3,7 @@ resource "aws_vpc" "main" {
   cidr_block         = var.cidr_block
   instance_tenancy   = "default"
   enable_dns_support = "true"
+  enable_dns_hostnames = "true"
 
   tags = merge(
     var.vpc_tags,
