c189003c446e5cd67a10c6ad49695f9d8b68b76c
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-05-09 21:47:05 +0530

    terraform infra aws vpc cretaed

--- variables.tf
+++ variables.tf
@@ -72,11 +72,11 @@ variable "database_route_table_tags" {
   default = {}
 }
 
-# variable "is_peering_required" {
-#   default = false
-# }
+variable "is_peering_required" {
+  default = false
+}
 
-# variable "vpc_peering_tags" {
-#   type    = map(string)
-#   default = {}
-# }
+variable "vpc_peering_tags" {
+  type    = map(string)
+  default = {}
+}
